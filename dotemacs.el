87a2e2e954337d2da176a49daf4f9df8d4cf1fcc
steini <steini90@gmail.com>
2013-09-19 00:23:50 +0000

    midnight mode

--- dotemacs.el
+++ dotemacs.el
@@ -49,6 +49,10 @@
 ;; ido-mode
 (ido-mode)
 
+;; midnight-mode
+(require 'midnight)
+(midnight-delay-set 'midnight-delay "4:30am")
+
 ;; smart-mode-line
 (add-to-load-path (initabspath "./lib/smart-mode-line"))
 (require 'smart-mode-line)
@@ -173,6 +177,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(clean-buffer-list-kill-buffer-names (quote ("*Help*" "*Apropos*" "*Man " "*Buffer List*" "*Compile-Log*" "*info*" "*vc*" "*vc-diff*" "*diff*" "^.+\\\\.py$" "*magit-.+*" "")))
  '(custom-safe-themes (quote ("dd4db38519d2ad7eb9e2f30bc03fba61a7af49a185edfd44e020aa5345e3dca7" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
